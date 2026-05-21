a5ea3e3ceeaa34f43a2741b18319031f5f7e4709
HousseinCh <houssein.ch@live.com>
2026-05-21 15:09:34 +0300

    :sparkles: Implement Splash Screen

--- app/src/main/java/com/example/qrcraft/app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/qrcraft/app/presentation/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.qrcraft.core.presentation.designsystem.theme.QRCraftTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             QRCraftTheme {
                 Scaffold(
